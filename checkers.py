ca921bf9b8a7b88ee83cea238679b396e689addb
Levi Remington <l.remington4415@student.sbccd.edu>
2019-04-23 18:43:08 -0700

    v4.7
    # Removed some debug print statements / polished code a little
    # changed checkers() function name to main() as per assignment requirements
    # fixed error where user could control enemy player's pieces if they attempted to move them the opposite direction
    # added win condition if player runs out of pieces
    # made grid() function to separately calculate map_grid for process()

--- checkers.py
+++ checkers.py
@@ -1,11 +1,13 @@
-# final project - checkers v4
+# final project - checkers v4.7
 # bijan - erik - john - levi
+
+# ============================================PROGRAM BELOW===============================================
 
 # Global scope - trying to limit this
 # references to move_list in beginning process() function and 'Long rect right - middle' in draw_board()
 move_list = []
 
-def checkers():
+def main():
   light_grid_color = makeColor(215, 215, 215)
   checkerboard = makeEmptyPicture(640,480)
   # This is the board with pieces in default position
@@ -19,24 +21,45 @@ def checkers():
                 [' ','b',' ','b',' ','b',' ','b'],
                 ['b',' ','b',' ','b',' ','b',' ']]
   repaint(draw_board(board_list, checkerboard))
-  
-  show_message('Welcome')
+  # Initiate game loop
+  show_message('WELCOME')
   gameover = false
   while not gameover:  
+    # Player 1
     board_list[:] = process(requestString("Player 1, make your move."), board_list, 0)
+    # Check for king
     for j in range(8):
       if board_list[0][j] == 'b':
-        print 'bk'
         board_list[0][j] = 'bk'
     repaint(draw_board(board_list, checkerboard))
+    # Check for Player 1 Win
+    num_white = 0
+    for i in range(8):
+      for j in range(8):
+        if 'w' in board_list[i][j] or 'wk' in board_list[i][j]:
+          num_white += 1
+    num_white = 0
+    if num_white == 0:
+      repaint(draw_board(board_list, checkerboard))
+      show_message('P1WINS')
+      return false
+    # Player 2
     board_list[:] = process(requestString("Player 2, make your move."), board_list, 1)
+    # Check for king
     for j in range(8):
       if board_list[7][j] == 'w':
-        print 'wk'
         board_list[7][j] = 'wk'
     repaint(draw_board(board_list, checkerboard))
-  
-  showInformation("GAME OVER")
+    # Check for Player 2 Win
+    num_black = 0
+    for i in range(8):
+      for j in range(8):
+        if 'b' in board_list[i][j] or 'bk' in board_list[i][j]:
+          num_black += 1
+    if num_black == 0:
+      repaint(draw_board(board_list, checkerboard))
+      show_message('P2WINS')
+      return false
 
 # Function to draw the board. Takes parameters of list board and initial blank image
 # Future plans: Add parameters to display Turn #, active player, etc.
@@ -118,26 +141,37 @@ def draw_board(text_board, img_board):
   for move in range(len(move_list)): # using global var 'move_list'
     addTextWithStyle(img_board, 525, 138+(18*move),str(move_list[move]), text_style1, black) # using global var 'move_list'
   
-  #small rect right - bottom
+  # Small rect right - bottom
   addRectFilled(img_board, 474, 384, 158, 70, light_grid_color)
   addRect(img_board, 474, 384, 158, 70, black)
   addTextWithStyle(img_board, 480, 405,"Player 1: " + str(num_black) + " pieces", text_style1, black)
   
+  # Check for gameover due to no pieces remaining
+  if num_white == 0:
+    show_message("P1WINS")
+    return exit
+  elif num_black == 0:
+    show_message("P2WINS")
+    return exit
+
   return img_board
+
 
 def show_message(prompt):
   prompt = prompt.lower()
   if prompt == 'welcome':
-    showInformation("Welcome to Checkers!\nIn this game, you and an opponent will take turns moving your pieces diagonally through text commands (e.g. 'C3 to D4').\nYour pieces can only move forward in a diagonal direction. However, pieces will be granted backwards movement capabilities if they reach the opposite end of the board.\nThe objective is to capture all of your enemy's pieces by jumping over them.\nRemember that you can string together multiple jumps!\nGood luck!")
+    showInformation("Welcome to Checkers!\nIn this game, you and an opponent will take turns moving your pieces diagonally through text commands (e.g. 'C3 to D4').\nYour pieces can only move forward in a diagonal direction. However, pieces will be granted backwards movement capabilities if they reach the opposite end of the board and become a King.\nThe objective is to capture all of your enemy's pieces by jumping over them.\nGood luck!")
   if prompt == 'invalid move':
     showInformation("Not a valid movement.")
   if prompt == 'error':
     showInformation("Error. Unable to process that command.")
+  if prompt == 'p1wins':
+    showInformation("Congratulations, Player 1. You win!")
+  if prompt == 'p2wins':
+    showInformation("Congratulations, Player 2. You win!")
 
-#Checks if a two spaces are diagonal and 'dist' spaces away
+#Checks if two spaces are diagonal and 'dist' spaces away
 def isAdjacent(startRow, startCol, destRow, destCol, dist):
-  #debugging print("Is it " + str(dist) + " Away: ")
-  #debugging print(str(abs(destRow - startRow) == dist and abs(destCol - startCol) == dist))
   if abs(destRow - startRow) == dist and abs(destCol - startCol) == dist:
     return true   
   return false
@@ -148,12 +182,10 @@ def isSpaceAnOpponentsPiece(row, col, player, board):
     if board[row][col] == 'w' or board[row][col] == 'wk':
       board[row][col] = ' '
       return true
-  
   if player == 1:
     if board[row][col] == 'b' or board[row][col] == 'bk':
       board[row][col] = ' '
       return true
-       
   return false
 
 #Checks if a space before has a piece to capture
@@ -165,14 +197,20 @@ def isPreviousDiagonalAPieceForCapture(startRow, startCol, destRow, destCol, pla
   colDist = (destCol - startCol) / 2
   middleRow = startRow + rowDist
   middleCol = startCol + colDist
-  #debugging print("jumped over enemy: ")
-  #debugging print (isSpaceAnOpponentsPiece(middleRow, middleCol, player, board))
   return isSpaceAnOpponentsPiece(middleRow, middleCol, player, board)
+
 
 def isMoveValid(startRow, startCol, destRow, destCol, board, player):
   #check if the board space has a piece and the destination is empty
   if board[startRow][startCol] == ' ' or board[destRow][destCol] != ' ':
     return false
+  #check if the starting piece belongs to player
+  if player == 0:
+    if board[startRow][startCol] == 'w' or board[startRow][startCol] == 'wk':
+      return false
+  if player == 1:
+    if board[startRow][startCol] == 'b' or board[startRow][startCol] == 'bk':
+      return false
   #check that the move is forward unless the piece is a King
   if board[startRow][startCol][-1:] != 'k':
     #player == 0 for black
@@ -181,25 +219,18 @@ def isMoveValid(startRow, startCol, destRow, destCol, board, player):
     #player == 1 for white
     if player == 1 and destRow < startRow:
       return false
-  
   #check that the spaces are adjacent Diagonals
   if isAdjacent(startRow, startCol, destRow, destCol, 1) == false:
     #if not an adjacent, check if they are two diagonals away and space between is a piece for a capture
     if isAdjacent(startRow, startCol, destRow, destCol, 2) and isPreviousDiagonalAPieceForCapture(startRow, startCol, destRow, destCol, player, board):
-      #debugging print ("capture")
       return true
     else:
-      #debugging print ("not capture")
       return false
   else:
     return true
-  
-
-# Process player movement commands (e.g. 'C3 to D4')
-# Adjusts board for proper movement display
-def process(command, text_board, player):
-  # Define grid dictionary for letter + num = x,y coordinates 
-  # to ease the calculation of user-input
+    
+# Define grid dictionary for letter + num = x,y coordinates to ease the calculation of user-input in process()
+def grid():
   map_grid = {}
   letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
   num = ['1', '2', '3', '4', '5', '6', '7', '8']
@@ -215,12 +246,18 @@ def process(command, text_board, player):
         map_grid[coord] = map_grid[coord].zfill(2)
       loc -= 10
     total_loc += 1
-  # Analyze user input
+  return map_grid
+  
+# Process player movement commands (e.g. 'C3 to D4')
+# Adjusts board for proper movement display
+def process(command, text_board, player):
+  map_grid = grid()
+  # Temp var for moving pieces
   piece = 'none'
   command_copy = command.lower()
   command = command.lower().split()
-  # Test if valid input
-  if len(command) == 3 and len(command[0]) == 2 and len(command[2]) == 2:
+  # Test if valid input (must be in format "a1 to b2")
+  if len(command) == 3 and len(command[0]) == 2 and len(command[2]) == 2 and command[0][:1].isalpha() and command[0][1:].isdigit() and command[2][:1].isalpha() and command[2][1:].isdigit():
     if command[1] == "to" and command[0][:1] <= 'h' and int(command[0][1:]) <= 8 and command[2][:1] <= 'h' and int(command[2][1:]) <= 8:
       # Test if valid movement for pieces 
       if command[0] in map_grid and command[2] in map_grid:
@@ -233,13 +270,13 @@ def process(command, text_board, player):
         y = int(map_grid[command[0]][1:])
         piece = text_board[x][y]
         text_board[x][y] = ' '
-        add_to_move_list(command_copy) # Add move to gloval variable 'move_list'
+        # Add move to global variable 'move_list'
+        add_to_move_list(command_copy) 
       # Place player piece
       if command[2] in map_grid:
         x = int(map_grid[command[2]][:1])
         y = int(map_grid[command[2]][1:])
-        text_board[x][y] = piece
-        
+        text_board[x][y] = piece  
       else:
         show_message('ERROR')
         return process(requestString("Player %d, make your move." % (player + 1)), text_board, player)
@@ -249,7 +286,7 @@ def process(command, text_board, player):
   else:
     show_message('ERROR')
     return process(requestString("Player %d, make your move." % (player + 1)), text_board, player)
-    
+  # Play audio of user's command  
   speech(command)
   
   return text_board
@@ -261,7 +298,7 @@ def add_to_move_list(command):
   if len(move_list) == 11:
     move_list.pop()
 
-# Function to play text-to-speech for player commands
+# Function to play text-to-speech audio for player commands
 def speech(command):
   # Folder must contain wav files for 'a', 'b', 'c', 'd', 'e', 'f', 'g', '1', '2', '3', '4', '5', '6', '7', '8', and 'to'
   folder = os.path.dirname(os.path.realpath(__file__)) + "\\sounds\\"
@@ -280,7 +317,7 @@ def speech(command):
   
   play(collage(speech1, speech2, speech3, speech4, speech5))
   
-# Combine audio files into one
+# Combine audio files into one for playback
 def collage(s1, s2, s3, s4, s5):
   totalLength = getLength(s1) + getLength(s2) + getLength(s3) + getLength(s4) + getLength(s5)
   final = makeEmptySound(totalLength, int(getSamplingRate(s1)))
